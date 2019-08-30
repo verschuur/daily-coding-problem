823cec644591371b13f07be18d0ede77b29ac730
Govert Verschuur <govert@noprotocol.nl>
2019-08-30 13:22:31 +0200

    Setup for problem 002

---
+++ src/P002/Solution.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Verschuur\DailyCodingProblem\P002;
+
+class Solution
+{
+    public function solve(array $items)
+    {
+        return $items;
+    }
+}
