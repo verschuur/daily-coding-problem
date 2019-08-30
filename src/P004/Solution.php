9d8a3a8a7425f595d9c68227bd909de7f3dcf4a2
Govert Verschuur <govert@noprotocol.nl>
2019-08-30 15:03:46 +0200

    Problem #004 initial solution

--- src/P004/Solution.php
+++ src/P004/Solution.php
@@ -7,6 +7,23 @@ class Solution
 {
     public function solve(array $items)
     {
-        return false;
+        // remove all negative integers and sort them ascending
+        $items = \array_filter($items, function ($item) {
+            return $item >= 1;
+        });
+        sort($items);
+
+        // Create a range between the first and last values
+        $range = range(reset($items), end($items));
+        $diff = array_diff($range, $items);
+        
+        // If there is a diff between the range and the items, get the first item and return it.
+        if (!empty($diff)) {
+            return reset($diff);
+        }
+        
+        // if there is no diff, then there is no break in the integer sequence of the array.
+        // Take the last value, increase it by one and return.
+        return end($items)+1;
     }
 }
