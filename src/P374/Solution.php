9c314789ba14d55eea9b18b774d59c072122af98
Govert Verschuur <govert@noprotocol.nl>
2019-11-29 11:33:15 +0100

    Finished problem 374

--- src/P374/Solution.php
+++ src/P374/Solution.php
@@ -5,8 +5,22 @@ namespace Verschuur\DailyCodingProblem\P374;
 
 class Solution
 {
-    public function solve(array $arr): int
+    public function solve(array $arr): ?int
     {
-        return 0;
+        // Sort the list going from smallest to largest integer for simplicity's sake.
+        asort($arr);
+
+        foreach ($arr as $key => $val) {
+            // Since we're looping from lowest to highest,
+            // as soon as the key and val match we're done.
+            if ($key === $val) {
+                return $key;
+            }
+            // If at this point no index has been found and we've reached the end of $arr,
+            // return null
+            if (array_key_last($arr) === $key) {
+                return null;
+            }
+        }
     }
 }
--- src/P374/SolutionTest.php
+++ src/P374/SolutionTest.php
@@ -10,5 +10,7 @@ class SolutionTest extends \PHPUnit\Framework\TestCase
         $solution = new Solution();
 
         $this->assertEquals(2, $solution->solve([-5, -3, 2, 3]));
+        $this->assertEquals(3, $solution->solve([-5, -3, 1, 3, 7]));
+        $this->assertEquals(7, $solution->solve([-5, -3, 1, 2, 7, 10, 99999, 7]));
     }
 }
