9f71458bfd6882816f42dda9f5536a5ef12aa577
Govert Verschuur <govert@noprotocol.nl>
2019-11-29 11:11:00 +0100

    Finished problem 373

--- src/P373/SolutionTest.php
+++ src/P373/SolutionTest.php
@@ -9,6 +9,13 @@ class SolutionTest extends \PHPUnit\Framework\TestCase
     {
         $solution = new Solution();
 
-        $this->assertTrue($solution->solve());
+        // sequence = 1, 2, 3, 4, 5 (length of 5)
+        $this->assertEquals(5, $solution->solve([5, 2, 99, 3, 4, 1, 100]));
+        
+        // sequence = 98, 99, 100, 101, 102, 103 (length of 6)
+        $this->assertEquals(6, $solution->solve([102, 5, 98, 2, 99, 3, 103, 4, 101, 1, 100]));
+
+        // sequence = 98, 99, 100 (length of 3)
+        $this->assertEquals(3, $solution->solve([102, 5, 98, 2, 99, 9, 103, 4, 101, 1, 105]));
     }
 }
