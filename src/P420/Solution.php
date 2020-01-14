579a945ed265724a7fefddd022eaff1dfcc21087
Govert Verschuur <govert@noprotocol.nl>
2020-01-14 11:05:01 +0100

    Finished problem 420

--- src/P420/Solution.php
+++ src/P420/Solution.php
@@ -3,10 +3,20 @@ declare(strict_types=1);
 
 namespace Verschuur\DailyCodingProblem\P420;
 
+use RuntimeException;
+
 class Solution
 {
     public function solve(int $input): int
     {
-        return 0;
+        if ($input < 0) {
+            throw new RuntimeException();
+        }
+
+        $values = \str_split((string)$input);
+        $sum = \array_sum($values);
+        $remainder = 10 - $sum;
+
+        return (int)($input . $remainder);
     }
 }
